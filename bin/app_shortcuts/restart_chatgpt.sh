fba7fd6870f7da934ea538d00d708f6de72418e3
Mor-Li <2568818204@qq.com>
2025-11-01 13:53:52 +0800

    fix: increase effiency

--- bin/app_shortcuts/restart_chatgpt.sh
+++ bin/app_shortcuts/restart_chatgpt.sh
@@ -3,5 +3,5 @@ pkill -f "/Applications/ChatGPT.app/Contents/MacOS/ChatGPT"
 # SpaceHopper 项目根目录
 : ${SPACEHOPPER_HOME:="/Users/limo/Documents/GithubRepo/SpaceHopper"}
 
-sleep 1
+sleep 0.1
 open /Applications/ChatGPT.app
